01cc83cb1c0ba8b7c3623d9917abea87a5d385c5
davarezza <dapva2728@gmail.com>
2023-12-23 18:54:19 +0700

    Feat: Add pagination and protection for API resource

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -91,9 +91,10 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($data as $item)
+                    <?php $i=$data['from']; ?>
+                    @foreach ($data['data'] as $item)
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $i }}</td>
                         <td>{{ $item['judul'] }}</td>
                         <td>{{ $item['pengarang'] }}</td>
                         <td>{{ date('d/m/Y',strtotime($item['tanggal_publikasi'])) }}</td>
@@ -106,9 +107,20 @@
                             </form>
                         </td>
                     </tr>
+                    <?php $i++; ?>
                     @endforeach
                 </tbody>
             </table>
+
+            @if ($data['links'])
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    @foreach ($data['links'] as $item)
+                        <li class="page-item {{ $item['active']?'active':'' }}"><a class="page-link" href="{{ $item['url2'] }}">{!! $item['label'] !!}</a></li>
+                    @endforeach
+                </ul>
+              </nav>
+            @endif
 
         </div>
         <!-- AKHIR DATA -->
